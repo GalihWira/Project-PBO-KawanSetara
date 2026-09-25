6ac752b06cf59c302cbd9be7738f5d19beffaa4c
Galih Wira <galihwira672@gmail.com>
2026-09-26 01:04:28 +0800

    Add: Nyoba-nyoba bikin tampilan login masih pemula puh

--- Project-PBO-KawanSetara/src/main/java/com/example/projectpbokawansetara/controller/LoginController.java
+++ Project-PBO-KawanSetara/src/main/java/com/example/projectpbokawansetara/controller/LoginController.java
@@ -1,14 +1,32 @@
 package com.example.projectpbokawansetara.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 
-public class LoginController {
+public class LoginController{
     @FXML
-    private Label welcomeText;
+    private TextField emailField;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private Label statusLabel;
+    @FXML
+    public void handleLoginButton(ActionEvent event) {
+        String user = emailField.getText();
+        String password = passwordField.getText();
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        if(user.isEmpty() || password.isEmpty()){
+            statusLabel.setText("Email atau Password harus diisi");
+            statusLabel.setStyle("-fx-text-fill: red");
+        }else{
+            statusLabel.setText("Login Berhasil");
+            statusLabel.setStyle("-fx-text-fill: green");
+        }
+        System.out.println("Email: " + user);
+        System.out.println("Password: " + password);
     }
 }
